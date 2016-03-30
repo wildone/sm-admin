1ff0fb189f1f27a1c5a0dbc02d563823e2d3f524
Bede Overend <bede.overend@gmail.com>
2016-03-30 16:37:47 +1100

    add gid / sid to body

--- src/sm-admin.js
+++ src/sm-admin.js
@@ -89,10 +89,32 @@ function inject() {
 
   // Namespace
   namespace = document.createElement('sm-block-namespace');
-  // TODO: In future, this should be changed to gid
-  namespace.global = true;
-  namespace.sid = namespace.uid = body.getAttribute('sid') || '';
+
   namespace.block = body;
+
+  // Define getters / setters for sid and gid
+  Object.defineProperty(body, 'sid', {
+    get() {
+      return this._smNamespace.gid;
+    },
+    set(value) {
+      this._smNamespace.gid = value;
+    },
+    enumerable: true
+  });
+
+  Object.defineProperty(body, 'gid', {
+    get() {
+      return this._smNamespace.gid;
+    },
+    set(value) {
+      this._smNamespace.gid = value;
+    },
+    enumerable: true
+  });
+
+  // Set initial value from attribute
+  body.gid = body.getAttribute('gid') || body.getAttribute('sid') || '';
 
   // Append admin
   admin = document.createElement('sm-admin');
